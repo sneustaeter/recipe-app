983c5d40dca3aabc3397104529c860560e3f04fd
sneustaeter <neustaeter.saxon@gmail.com>
2021-05-31 16:54:08 -0600

    Add files via upload

--- src/main/java/com/sneustaeter/recipeapp/Recipe.java
+++ src/main/java/com/sneustaeter/recipeapp/Recipe.java
@@ -12,6 +12,9 @@ public class Recipe {
     public String description = "";
     public String[] ingredients;
     public String[] steps;
+    int prepTime = 0; // Times are in minutes
+    int cookTime = 0; // Times are in minutes
+    int totalTime = 0;
     /*The tags array will hold many single word/short phrase tags used to search and index
     the recipes*/
     public String[] tags;
@@ -22,14 +25,21 @@ public class Recipe {
     }
     
     //Constructor that takes in basic recipe information
-    public Recipe(String nameIn, String descriptionIn, String[] ingredientsIn, String[] stepsIn){
+    public Recipe(String nameIn, String descriptionIn, String[] ingredientsIn, String[] stepsIn, int prepTimeIn, int cookTimeIn){
         name = nameIn;
         description = descriptionIn;
         ingredients = ingredientsIn;
         steps = stepsIn;
+        prepTime = prepTimeIn;
+        cookTime = cookTimeIn;
+        
+        updateTotalTime();
     }
     
     
+    
+    /* Getters and setters start here */
+    /* ============================== */
     public String getName(){
         return name;
     }
@@ -61,6 +71,21 @@ public class Recipe {
         steps = stepsIn;
     }
     
+    public int getPrepTime(){
+        return prepTime;
+    }
+    public void setPrepTime(int prepTimeIn){
+        prepTime = prepTimeIn;
+    }
+    
+    
+    public int getCookTime(){
+        return cookTime;
+    }
+    public void setCookTime(int cookTimeIn){
+        cookTime = cookTimeIn;
+    }
+    
     
     public String[] getTags(){
         return tags;
@@ -68,6 +93,9 @@ public class Recipe {
     public void setTags(String[] tagsIn){
         tags = tagsIn;
     }
+    
+    /* Getters and setters end here */
+    /* ============================== */
     
     
     
@@ -77,7 +105,29 @@ public class Recipe {
         
         String outStr = "Name: " + name;
         outStr += "\n\tDescription: " + description;
+        outStr += "\n\tPrep Time: " + prepTime + " minutes\tCook Time: " + cookTime + " minutes\tTotal Time: " + totalTime + " minutes";
+        outStr += "\n\tIngredients:";
+        
+        //For loop to process ingredient array
+        for (int i = 0; i < ingredients.length; i++){
+            outStr += "\n\t\t" + ingredients[i];
+        }
+        
+        outStr += "\n\tDirections: ";
+        
+        //for loop to process steps array
+        for(int i = 0; i < steps.length; i++){
+            outStr += "\n\t\t" + steps[i];
+        }
+        
         //FINISH ME
         return outStr;
+    }
+    
+    
+    
+    // Calculates and updates the total time needed for a recipe
+    public void updateTotalTime(){
+        totalTime = prepTime + cookTime;
     }
 }
--- src/main/java/com/sneustaeter/recipeapp/Runner.java
+++ src/main/java/com/sneustaeter/recipeapp/Runner.java
@@ -1,5 +1,8 @@
 package com.sneustaeter.recipeapp;
 
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 public class Runner {
 
     /**
@@ -7,6 +10,12 @@ public class Runner {
      */
     public static void main(String[] args) {
         //Do some shit
+        
+        //Scanner scr = new Scanner(System.in);
+        String name = JOptionPane.showInputDialog(null, "Please enter the name of the recipe" );
+        String description = JOptionPane.showInputDialog(null, "Please enter a breif description of the recipe");
+        int prepTime = Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter the time required for prep"));
+        int cookTime = Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter the time required for cooking"));
     }
 
 }
